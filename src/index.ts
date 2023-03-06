311786c3ee569e6a1fe2b4e39eca7a45a628d53b
hanchchch <cndghks15@gmail.com>
2023-03-06 22:59:55 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { OpenAIModule } from "./openai/openai.module";
+export { OpenAIService } from "./openai/openai.service";
+export {
+  OpenAIModuleOptions,
+  OpenAIModuleAsyncOptions,
+} from "./openai/module-options.interface";
